04a2eec3e6a808394341e3c5016b912232799b59
Novozhilov Konstantin <kostyansa@gmail.com>
2022-08-15 12:07:35 +0400

    Update wsgi.py

--- wsgi.py
+++ wsgi.py
@@ -1,7 +1,7 @@
 from prometheus_client import make_wsgi_app
 
 
-def app(environ, start_fn):
+def application(environ, start_fn):
     if environ['PATH_INFO'] == '/metrics':
         metrics_app = make_wsgi_app()
         return metrics_app(environ, start_fn)
