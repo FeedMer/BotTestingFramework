f7822381518d4470a9889a64cb8fd3ded708fba8
Novozhilov Konstantin <kostyansa@gmail.com>
2022-09-26 15:50:30 +0400

    [ADD] docker support

--- wsgi.py
+++
@@ -1,7 +0,0 @@
-from prometheus_client import make_wsgi_app
-
-
-def application(environ, start_fn):
-    if environ['PATH_INFO'] == '/metrics':
-        metrics_app = make_wsgi_app()
-        return metrics_app(environ, start_fn)
