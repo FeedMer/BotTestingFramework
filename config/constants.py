9c094a479deab13c9af9adc6e4080107c5dd74d4
Novozhilov Konstantin <kostyansa@gmail.com>
2023-05-26 16:28:44 +0400

    Update constants.py

--- config/constants.py
+++ config/constants.py
@@ -4,17 +4,15 @@ import configparser
 
 
 class Constants:
-    _config = configparser.ConfigParser()
-    _config.read("resources/config.ini")
 
-    TEST_TIMEOUT = int(os.environ.get("TEST_TIMEOUT") or _config["DEFAULT"]["TEST_TIMEOUT"])
-    ERROR_TIMEOUT = int(os.environ.get("ERROR_TIMEOUT") or _config["DEFAULT"]["ERROR_TIMEOUT"])
-    TEST_INTERVAL = int(os.environ.get("TEST_INTERVAL") or _config["DEFAULT"]["TEST_INTERVAL"])
-    CLEANUP_INTERVAL = int(os.environ.get("CLEANUP_INTERVAL") or _config["DEFAULT"]["CLEANUP_INTERVAL"])
-    API_ID = os.environ.get("API_ID") or _config["DEFAULT"]["API_ID"]
-    API_HASH = os.environ.get("API_HASH") or _config["DEFAULT"]["API_HASH"]
-    ACCOUNT = os.environ.get("ACCOUNT") or _config["DEFAULT"]["ACCOUNT"]
-    API_KEY_BOT = os.environ.get("API_KEY_BOT") or _config["DEFAULT"]["API_KEY_BOT"]
-    LOG_DB_URL = os.environ.get("LOG_DB_URL") or _config["DEFAULT"]["DB_URL"]
-    FEEDMER_DB_URL = os.environ.get("FEEDMER_DB_URL") or _config["DEFAULT"]["DB_URL"]
+    TEST_TIMEOUT = int(os.environ.get("TEST_TIMEOUT"))
+    ERROR_TIMEOUT = int(os.environ.get("ERROR_TIMEOUT"))
+    TEST_INTERVAL = int(os.environ.get("TEST_INTERVAL"))
+    CLEANUP_INTERVAL = int(os.environ.get("CLEANUP_INTERVAL"))
+    API_ID = os.environ.get("API_ID")
+    API_HASH = os.environ.get("API_HASH")
+    ACCOUNT = os.environ.get("ACCOUNT")
+    API_KEY_BOT = os.environ.get("API_KEY_BOT")
+    LOG_DB_URL = os.environ.get("LOG_DB_URL")
+    FEEDMER_DB_URL = os.environ.get("FEEDMER_DB_URL")
 
