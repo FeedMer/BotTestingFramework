fa7f4a6484c3b24a5d5335cbb59c048f11ed3ee0
Novozhilov Konstantin <kostyansa@gmail.com>
2023-06-05 16:33:27 +0400

    Update main.py

--- main.py
+++ main.py
@@ -40,7 +40,7 @@ async def schedule():
         scenario = data["scenario"]
         logging.info(f"Manager chat id :{test.manager.id}")
         for api_key in bots.get_api_keys():
-            await asyncio.sleep(60)
+            await asyncio.sleep(30)
             bot = await telegram.get_bot_address(api_key)
             name = bot.username
             scheduler.add_job(
